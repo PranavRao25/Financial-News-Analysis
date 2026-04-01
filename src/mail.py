c4beff7dd4cc7572d5d9f9ceec08a08ee608c309
Pranav Rao <pranavrao2500@gmail.com>
2026-04-01 16:01:26 +0530

    post monitoring v1 done

---
+++ src/mail.py
@@ -0,0 +1,70 @@
+from flask import Flask, request, jsonify
+import yaml
+from pathlib import Path
+import logging
+import json
+import mailtrap as mt
+import os
+
+app = Flask(__name__)
+parent = Path(__file__).resolve().parent.parent
+
+with open(parent / "config.yaml", "r") as f:
+    raw_path = f.read()
+expanded_yaml = os.path.expandvars(raw_path)
+configs = yaml.full_load(expanded_yaml)
+
+port = configs["monitoring"]["port"]["alert"]
+
+SENDER_EMAIL = configs["mail_alerts"]["sender_mail"]
+RECEIVER_EMAIL = configs["mail_alerts"]["receiver_mail"]
+MAIL_API = configs["mail_alerts"]["api"]
+
+def send_mail(subject, body):
+    try:
+        mail = mt.Mail(
+            sender=mt.Address(email=SENDER_EMAIL, name="Alertmanager"),
+            to=[mt.Address(email=RECEIVER_EMAIL)],
+            subject=subject,
+            text=body
+        )
+
+        client = mt.MailtrapClient(token=MAIL_API)
+        response = client.send(mail)
+        return response
+    except Exception as e:
+        print(f"Failed with exception : {e}")
+
+@app.route('/', methods = ["POST"])
+def webhook():
+    if request.method == "POST":
+        try:
+            payload = request.json
+        except json.JSONDecodeError:
+            return "Invalid json", 400
+
+        if payload and "alerts" in payload:
+            for alert in payload["alerts"]:
+                status = alert.get("status", "Unknown")
+                labels = alert.get("labels", {})
+                annotations = alert.get("annotations", {})
+
+                alertname = labels.get("alertname", "Unnamed Alert")
+                severity = labels.get("severity", "unknown")
+                summary = annotations.get("summary", "no summary")
+                description = annotations.get("description", "no description")
+
+                subject = f"[{status.upper()}] {severity.upper()} Alert : {alertname}"
+                body = (
+                    f"Alert Status: {status}\n"
+                    f"Alert Name: {alertname}\n"
+                    f"Summary: {summary}\n"
+                    f"Description: {description}\n"
+                    f"Payload: {alert}\n"
+                )
+                _ = send_mail(subject, body)
+        
+    return jsonify({"status": "success"}), 200
+    
+if __name__ == "__main__":
+    app.run(debug=True, port = port)
